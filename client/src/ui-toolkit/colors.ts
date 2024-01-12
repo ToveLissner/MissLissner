ea2054a5ae7965e95931a6bfb7d9aa3edf718578
Tove Lissner <114910395+ToveLissner@users.noreply.github.com>
2024-01-12 10:29:40 +0100

    feat(client): adding colors in colors.ts

---
+++ client/src/ui-toolkit/colors.ts
@@ -0,0 +1,15 @@
+const colors = {
+  V75background: "rgb(0, 79, 159)",
+  V86background: "rgb(113, 25, 112)",
+  V64background: "rgb(243, 147, 50)",
+  GS75background: "rgb(42, 123, 89)",
+  V64backgroundMarked: "rgb(233, 90, 12)",
+  V86backgroundMarked: "rgb(145, 68, 143)",
+  V75backgroundMarked: "rgb(13, 87, 161)",
+  GS75backgroundMarked: "rgb(42, 123, 89)",
+  blueBackgroundColor: "#014F9F",
+  pinkBackgroundColor: "rgb(215, 35, 189)",
+  pinkColor: "pink",
+};
+
+export default colors;
