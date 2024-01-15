ab6c129c248bd364ee1409760406aae856c14de5
Tove Lissner <114910395+ToveLissner@users.noreply.github.com>
2024-01-15 12:33:20 +0100

    feat(client): create SignUpButton component

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useSelector } from "react-redux";
-import SignUp from "../SignUp/SignUp";
+import SignUpButton from "../SignUp/SignUpButton";
 import User from "../User/User";
 import colors from "../../ui-toolkit/colors";
 import { RootState } from "../../domain/store";
@@ -24,7 +24,7 @@ const Header: React.FC = () => {
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           MissLissner
         </Typography>
-        {!isLoggedIn && <SignUp />}
+        {!isLoggedIn && <SignUpButton />}
         <User />
       </Toolbar>
     </AppBar>
---
+++ client/src/components/SignUp/SignUpButton.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { Button } from "@mui/material";
+import SignUpModal from "./SignUpModal";
+
+const SignUpButton = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <Button
+        variant="contained"
+        color="success"
+        onClick={handleOpenModal}
+        sx={{ textTransform: "none", fontSize: "14px" }}
+      >
+        Öppna konto
+      </Button>
+      <SignUpModal open={isModalOpen} onClose={handleCloseModal} />
+    </>
+  );
+};
+
+export default SignUpButton;
