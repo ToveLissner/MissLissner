4f01c90f75dfa20eb8ba39bbab75095610afbb8f
Tove Lissner <114910395+ToveLissner@users.noreply.github.com>
2024-01-15 15:01:06 +0100

    feat(client): integrate CustomSnackbar in LogInModal to display success message

--- client/src/components/User/LogInModal.tsx
+++ client/src/components/User/LogInModal.tsx
@@ -6,6 +6,7 @@ import { ThunkDispatch } from "redux-thunk";
 import { Action } from "@reduxjs/toolkit";
 import { RootState } from "../../domain/store";
 import CustomModal from "../../ui-toolkit/components/CustomModal";
+import CustomSnackbar from "../../ui-toolkit/components/CustomSnackbar";
 
 type LogInModalProps = {
   open: boolean;
@@ -19,6 +20,11 @@ const LogInModal: React.FC<LogInModalProps> = ({ open, onClose }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarSeverity, setSnackbarSeverity] = useState<"success" | "error">(
+    "success"
+  );
+  const [snackbarMessage, setSnackbarMessage] = useState("");
   const isLoggedIn = useSelector(
     (state: RootState) => state.user.data.isLoggedIn
   );
@@ -57,130 +63,62 @@ const LogInModal: React.FC<LogInModalProps> = ({ open, onClose }) => {
   };
 
   useEffect(() => {
-    // Kolla om användaren är inloggad - stäng modalen om det är true
     if (isLoggedIn) {
       onClose();
+      setSnackbarSeverity("success");
+      setSnackbarMessage("Inloggningen lyckades!");
+      setSnackbarOpen(true);
     }
   }, [isLoggedIn, onClose]);
 
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+  };
+
   return (
-    <CustomModal
-      open={open}
-      onClose={onClose}
-      onConfirm={() => {}}
-      title="Logga in"
-      content={
-        <>
-          <TextField
-            label="Användarnamn"
-            variant="outlined"
-            fullWidth
-            value={username}
-            onChange={handleInputUsername}
-            margin="normal"
-          />
-          <TextField
-            label="Lösenord"
-            variant="outlined"
-            fullWidth
-            type="password"
-            value={password}
-            onChange={handleInputPassword}
-            margin="normal"
-          />
-          {error && (
-            <Typography variant="body2" color="error" sx={{ mt: 1 }}>
-              {error}
-            </Typography>
-          )}
-        </>
-      }
-      buttonText="Logga in"
-      onButtonClick={handleLogIn}
-    />
+    <>
+      <CustomModal
+        open={open}
+        onClose={onClose}
+        onConfirm={() => {}}
+        title="Logga in"
+        content={
+          <>
+            <TextField
+              label="Användarnamn"
+              variant="outlined"
+              fullWidth
+              value={username}
+              onChange={handleInputUsername}
+              margin="normal"
+            />
+            <TextField
+              label="Lösenord"
+              variant="outlined"
+              fullWidth
+              type="password"
+              value={password}
+              onChange={handleInputPassword}
+              margin="normal"
+            />
+            {error && (
+              <Typography variant="body2" color="error" sx={{ mt: 1 }}>
+                {error}
+              </Typography>
+            )}
+          </>
+        }
+        buttonText="Logga in"
+        onButtonClick={handleLogIn}
+      />
+      <CustomSnackbar
+        open={snackbarOpen}
+        onClose={handleSnackbarClose}
+        severity={snackbarSeverity}
+        message={snackbarMessage}
+      />
+    </>
   );
 };
 
 export default LogInModal;
-
-// import React, { useState } from "react";
-// import { Modal, Box, Typography, TextField, Button } from "@mui/material";
-// import { useDispatch } from "react-redux";
-// import { logInAsync } from "../../domain/user/userSlice";
-// import { ThunkDispatch } from "redux-thunk";
-// import { Action } from "@reduxjs/toolkit";
-// import { RootState } from "../../domain/store";
-
-// type LogInModalProps = {
-//   open: boolean;
-//   onClose: () => void;
-// };
-
-// type AppThunk = ThunkDispatch<RootState, null, Action<string>>;
-
-// const LogInModal: React.FC<LogInModalProps> = ({ open, onClose }) => {
-//   const dispatch = useDispatch<AppThunk>();
-//   const [username, setUsername] = useState("");
-//   const [password, setPassword] = useState("");
-
-//   const handleInputUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     setUsername(event.target.value);
-//   };
-
-//   const handleInputPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     setPassword(event.target.value);
-//   };
-
-//   const handleLogIn = async () => {
-//     try {
-//       console.log("Före dispatch");
-
-//       const loginAction = logInAsync({ username, password });
-//       await dispatch(loginAction);
-
-//       console.log("Efter dispatch");
-
-//       // Stäng modalen efter inloggning
-//       onClose();
-//     } catch (error) {
-//       console.error("Failed to log in:", error);
-//     }
-//   };
-
-//   return (
-//     <>
-//       <Modal
-//         open={open}
-//         onClose={onClose}
-//         aria-labelledby="simple-modal-title"
-//         aria-describedby="simple-modal-description"
-//       >
-//         <Box>
-//           <Typography variant="h6">Logga in</Typography>
-//           <TextField
-//             label="Användarnamn"
-//             variant="outlined"
-//             fullWidth
-//             value={username}
-//             onChange={handleInputUsername}
-//             margin="normal"
-//           />
-//           <TextField
-//             label="Lösenord"
-//             variant="outlined"
-//             fullWidth
-//             type="password"
-//             value={password}
-//             onChange={handleInputPassword}
-//             margin="normal"
-//           />
-//           <Button variant="contained" color="primary" onClick={handleLogIn}>
-//             Logga in
-//           </Button>
-//         </Box>
-//       </Modal>
-//     </>
-//   );
-// };
-
-// export default LogInModal;
