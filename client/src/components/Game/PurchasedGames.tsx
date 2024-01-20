800051595dfe9e41108d4f17588eec86cf691b8a
Tove Lissner <114910395+ToveLissner@users.noreply.github.com>
2024-01-20 22:41:27 +0100

    feat(client): handle displaying purchased games based on user login status
    - update PurchasedGames component to conditionally render game list or message based on user login status
    - add check for user login status in PurchasedGames component

--- client/src/components/Game/PurchasedGames.tsx
+++ client/src/components/Game/PurchasedGames.tsx
@@ -6,27 +6,30 @@ import GameCard from "./GameCard";
 
 const PurchasedGames: React.FC = () => {
   const gameData = useSelector((state: RootState) => state.game.games);
+  const userData = useSelector((state: RootState) => state.user.data);
 
   return (
     <>
       <Box sx={{ padding: 1, backgroundColor: "rgb(236, 236, 237)" }}>
-        {gameData.length > 0 ? (
-          gameData.map((game) => <GameCard key={game.gameID} game={game} />)
-        ) : (
-          <Box
-            sx={{
-              backgroundColor: "white",
-              borderRadius: "8px",
-              padding: "16px",
-              margin: "16px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Typography>Inga kvitton hittades</Typography>
-          </Box>
-        )}
+        {userData.isLoggedIn ? (
+          gameData.length > 0 ? (
+            gameData.map((game) => <GameCard key={game.gameID} game={game} />)
+          ) : (
+            <Box
+              sx={{
+                backgroundColor: "white",
+                borderRadius: "8px",
+                padding: "16px",
+                margin: "16px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Typography>Inga kvitton hittades</Typography>
+            </Box>
+          )
+        ) : null}
       </Box>
     </>
   );
