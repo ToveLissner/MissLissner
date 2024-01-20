8b87a459970e8a42867f7733ab2bd375f88982d4
Tove Lissner <114910395+ToveLissner@users.noreply.github.com>
2024-01-20 22:24:51 +0100

    style(client): ImagesPage

--- client/src/components/Pages/ImagesPage.tsx
+++ client/src/components/Pages/ImagesPage.tsx
@@ -1,11 +1,27 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Paper, Typography } from "@mui/material";
 
 const ImagesPage: React.FC = () => {
+  const heroImageStyle = {
+    backgroundImage: `url(${process.env.PUBLIC_URL}/images/3.jpeg)`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    height: "60vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "white",
+    padding: "20px",
+  };
   return (
     <Box>
-      <Typography variant="h1">Bildsida</Typography>
-      <img src={process.env.PUBLIC_URL + "/images/1.jpeg"} alt="En bild" />
+      <Paper sx={heroImageStyle}>
+        <Typography variant="h3">För dig som vill vara med i spelet</Typography>
+      </Paper>
+      <Box>
+        <img src={process.env.PUBLIC_URL + "/images/1.jpeg"} alt="En bild" />
+      </Box>
     </Box>
   );
 };
