d793c539fb23d68e34ea4efc108842975591da1c
Tove Lissner <114910395+ToveLissner@users.noreply.github.com>
2024-01-20 22:05:45 +0100

    feat(client): create GameCard component

---
+++ client/src/components/Game/GameCard.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Game } from "../../models/Game";
+import { Box, Typography } from "@mui/material";
+
+type GameCardProps = {
+  game: Game;
+};
+
+const formatPurchaseDate = (purchaseDate: string): string => {
+  const date = new Date(purchaseDate);
+  const formattedDate = date.toLocaleDateString("sv-SE", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  });
+
+  return formattedDate;
+};
+
+const GameCard: React.FC<GameCardProps> = ({ game }) => {
+  return (
+    <Box
+      sx={{
+        backgroundColor: "white",
+        borderRadius: "8px",
+        padding: "16px",
+        margin: "16px",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <Box>
+        <Typography variant="body1">{`Speltyp: ${game.gameTypeID}`}</Typography>
+      </Box>
+      <Box sx={{ textAlign: "right", backgroundColor: "white" }}>
+        <Typography variant="body2">{`Kostnad: ${game.price} kr`}</Typography>
+        <Typography
+          variant="body2"
+          sx={{ fontWeight: "bold" }}
+        >{`Inlämnad: ${formatPurchaseDate(game.purchaseDate)}`}</Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default GameCard;
