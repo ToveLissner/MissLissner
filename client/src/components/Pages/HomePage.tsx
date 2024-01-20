f6b20b9b1b4cb5fb5fd226db7e4a641cbd31ed87
Tove Lissner <114910395+ToveLissner@users.noreply.github.com>
2024-01-20 22:22:02 +0100

    style(client): styling HomePage

--- client/src/components/Pages/HomePage.tsx
+++ client/src/components/Pages/HomePage.tsx
@@ -1,13 +1,44 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Paper } from "@mui/material";
 import GamesToPlay from "../Game/GamesToPlay";
 
 const HomePage: React.FC = () => {
+  const heroImageStyle = {
+    backgroundImage: `url(${process.env.PUBLIC_URL}/images/3.jpeg)`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    height: "60vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "white",
+    padding: "20px",
+  };
+
+  const heroImageStyle2 = {
+    backgroundImage: `url(${process.env.PUBLIC_URL}/images/7.jpeg)`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    height: "400px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "white",
+    padding: "20px",
+  };
+
   return (
     <Box>
-      {/* <Typography variant="h1">Välkommen till startsidan!</Typography> */}
-      <img src={process.env.PUBLIC_URL + "/images/1.jpeg"} alt="En bild" />
+      <Paper sx={heroImageStyle}>
+        <Typography variant="h3">För dig som vill vara med i spelet</Typography>
+        <Typography variant="h4"> - men gärna unviker jobbet</Typography>
+      </Paper>
       <GamesToPlay />
+      <Paper sx={heroImageStyle2}>
+        <Typography variant="h3">Låt spelet starta</Typography>
+      </Paper>
     </Box>
   );
 };
