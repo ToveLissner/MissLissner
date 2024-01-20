858fb0f4fadee6c3d168dc6b32d442dc631908b7
Tove Lissner <114910395+ToveLissner@users.noreply.github.com>
2024-01-20 22:18:39 +0100

    feat(client): import Link from "react-router-dom" in Header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -7,6 +7,7 @@ import User from "../User/User";
 import colors from "../../ui-toolkit/colors";
 import { RootState } from "../../domain/store";
 import SideMenu from "./SideMenu";
+import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
   const [isSideMenuOpen, setSideMenuOpen] = useState(false);
@@ -31,10 +32,12 @@ const Header: React.FC = () => {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            MissLissner
-          </Typography>
           {!isLoggedIn && <SignUpButton />}
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+              MissLissner
+            </Link>
+          </Typography>
           <User />
         </Toolbar>
       </AppBar>
