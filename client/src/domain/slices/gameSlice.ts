d9412ff0e629b58420a0a62fc39b25acbc52543e
Tove Lissner <114910395+ToveLissner@users.noreply.github.com>
2024-01-22 16:31:09 +0100

    cleanup(client): remove commented-out code in gameSlice

--- client/src/domain/slices/gameSlice.ts
+++ client/src/domain/slices/gameSlice.ts
@@ -19,25 +19,6 @@ const initialState: GameState = {
   error: null,
 };
 
-// const initialState: GameState = {
-//   games: [{ gameID: 0, price: 0, purchaseDate: "", gameTypeID: 0, userID: 0 }],
-//   loading: false,
-//   error: null,
-// };
-
-// export const createGameAsync = (
-//   game: Game
-// ): ThunkAction<void, RootState, unknown, PayloadAction<Game>> => {
-//   return async (dispatch: Dispatch) => {
-//     try {
-//       const newGame = await createGameService(game);
-//       dispatch(addGame(newGame));
-//     } catch (error) {
-//       console.error("Failed to create game:", error);
-//     }
-//   };
-// };
-
 export const createGameAsync = (
   game: Game
 ): ThunkAction<void, RootState, unknown, PayloadAction<Game>> => {
@@ -45,7 +26,6 @@ export const createGameAsync = (
     try {
       const newGame = await createGameService(game);
 
-      // Använd addGame för att lägga till eller uppdatera spelet i listan
       dispatch(addGame(newGame));
     } catch (error) {
       console.error("Failed to create game:", error);
@@ -78,33 +58,6 @@ const gameSlice = createSlice({
         error: null,
       };
     },
-
-    // addGame: (state, action: PayloadAction<Game>) => {
-    //   // Kontrollera om spelet redan finns i listan
-    //   const existingGameIndex = state.games.findIndex(
-    //     (game) => game.gameID === action.payload.gameID
-    //   );
-
-    //   if (existingGameIndex === -1) {
-    //     return {
-    //       ...state,
-    //       games: [...state.games, action.payload],
-    //       loading: false,
-    //       error: null,
-    //     };
-    //   } else {
-    //     // Om spelet redan finns, uppdatera det istället för att lägga till en ny kopia
-    //     const updatedGames = [...state.games];
-    //     updatedGames[existingGameIndex] = action.payload;
-
-    //     return {
-    //       ...state,
-    //       games: updatedGames,
-    //       loading: false,
-    //       error: null,
-    //     };
-    //   }
-    // },
 
     addGame: (state, action: PayloadAction<Game>) => {
       const existingGameIndex = state.games.findIndex(
