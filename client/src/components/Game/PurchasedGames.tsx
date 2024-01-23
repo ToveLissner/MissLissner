541bf9d54e4292dcf7567be144fec9c3cf84304f
Tove Lissner <114910395+ToveLissner@users.noreply.github.com>
2024-01-23 22:06:41 +0100

    feat(client): implement pagination in PurchasedGames

--- client/src/components/Game/PurchasedGames.tsx
+++ client/src/components/Game/PurchasedGames.tsx
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../domain/store";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Pagination } from "@mui/material";
 import GameCard from "./GameCard";
+
+const PAGE_SIZE = 10;
 
 const PurchasedGames: React.FC = () => {
   const gameData = useSelector((state: RootState) => state.game.games);
   const userData = useSelector((state: RootState) => state.user.data);
 
-  return (
-    <>
-      <Box sx={{ padding: 1, backgroundColor: "rgb(236, 236, 237)" }}>
-        <Box sx={{ textAlign: "left", padding: 2, paddingBottom: 0 }}>
-          {" "}
-          <Typography variant="h4">Spelkvitton</Typography>
-        </Box>
+  const [currentPage, setCurrentPage] = useState(1);
 
-        {userData.isLoggedIn ? (
-          gameData.length > 0 ? (
-            gameData.map((game) => <GameCard key={game.gameID} game={game} />)
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    setCurrentPage(value);
+  };
+
+  const paginatedGames = gameData.slice(
+    (currentPage - 1) * PAGE_SIZE,
+    currentPage * PAGE_SIZE
+  );
+
+  return (
+    <Box sx={{ padding: 1, backgroundColor: "rgb(236, 236, 237)" }}>
+      <Box sx={{ textAlign: "left", padding: 2, paddingBottom: 0 }}>
+        <Typography variant="h4">Spelkvitton</Typography>
+      </Box>
+
+      {userData.isLoggedIn ? (
+        <Box>
+          {gameData.length > 0 ? (
+            paginatedGames.map((game) => (
+              <GameCard key={game.gameID} game={game} />
+            ))
           ) : (
             <Box
               sx={{
@@ -33,26 +50,42 @@ const PurchasedGames: React.FC = () => {
             >
               <Typography>Inga kvitton hittades</Typography>
             </Box>
-          )
-        ) : (
-          <Box
-            sx={{
-              backgroundColor: "white",
-              borderRadius: "8px",
-              padding: "16px",
-              margin: "16px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Typography>
-              För att kunna se dina spelkvitton måste du vara inloggad
-            </Typography>
-          </Box>
-        )}
+          )}
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            backgroundColor: "white",
+            borderRadius: "8px",
+            padding: "16px",
+            margin: "16px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Typography>
+            För att kunna se dina spelkvitton måste du vara inloggad
+          </Typography>
+        </Box>
+      )}
+
+      <Box
+        sx={{
+          textAlign: "center",
+          marginTop: 2,
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Pagination
+          count={Math.ceil(gameData.length / PAGE_SIZE)}
+          page={currentPage}
+          onChange={handlePageChange}
+          color="primary"
+        />
       </Box>
-    </>
+    </Box>
   );
 };
 
