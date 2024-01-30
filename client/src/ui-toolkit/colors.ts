dd91a1503553c1678ff9619498c5345ad5e376ec
Tove Lissner <114910395+ToveLissner@users.noreply.github.com>
2024-01-30 14:52:34 +0100

    chore(client): update color definitions in colors.ts

--- client/src/ui-toolkit/colors.ts
+++ client/src/ui-toolkit/colors.ts
@@ -6,14 +6,16 @@ const colors: Colors = {
   V75background: "rgb(0, 79, 159)",
   V86background: "rgb(113, 25, 112)",
   V64background: "rgb(243, 147, 50)",
+  V65background: "rgb(192, 10, 38)",
   GS75background: "rgb(42, 123, 89)",
+  V4background: "rgb(76, 175, 230)",
+  V4backgroundMarked: "rgb(41, 141, 205)",
   V64backgroundMarked: "rgb(233, 90, 12)",
+  V65backgroundMarked: "rgb(192, 10, 38)",
   V86backgroundMarked: "rgb(145, 68, 143)",
   V75backgroundMarked: "rgb(13, 87, 161)",
   GS75backgroundMarked: "rgb(42, 123, 89)",
-  blueBackgroundColor: "#014F9F",
   pinkBackgroundColor: "rgb(215, 35, 189)",
-  pinkColor: "pink",
 };
 
 export default colors;
