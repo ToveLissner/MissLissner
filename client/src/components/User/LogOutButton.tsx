58734b7c6b23cdd82637c280292aaaaca79679c8
Tove Lissner <114910395+ToveLissner@users.noreply.github.com>
2024-01-30 14:14:31 +0100

    feat(client): enhance LogOutButton functionality and styling
    - add redirection to the homepage after logout.
    - adjust styling for a better visual experience.

--- client/src/components/User/LogOutButton.tsx
+++ client/src/components/User/LogOutButton.tsx
@@ -1,24 +1,29 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { logOut } from "../../domain/user/userSlice";
+import { useNavigate } from "react-router-dom";
+import { logOut } from "../../domain/slices/userSlice";
 import { Button } from "@mui/material";
 
 const LogOutButton: React.FC<{ onClick: () => void }> = ({ onClick }) => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     dispatch(logOut());
+    navigate("/");
     onClick();
   };
 
   return (
-    <Button
-      variant="outlined"
-      onClick={handleLogout}
-      sx={{ textTransform: "none" }}
-    >
-      Logga ut
-    </Button>
+    <>
+      <Button
+        variant="outlined"
+        onClick={handleLogout}
+        sx={{ mt: 1, textTransform: "none", width: "100%", height: "50px" }}
+      >
+        Logga ut
+      </Button>
+    </>
   );
 };
 
