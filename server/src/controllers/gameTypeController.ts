edc11c261a867349be9ac1955242d2605fdc75ec
Tove Lissner <114910395+ToveLissner@users.noreply.github.com>
2024-02-03 13:53:40 +0100

    cleanup(server): remove commented-out code

--- server/src/controllers/gameTypeController.ts
+++ server/src/controllers/gameTypeController.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express";
 import {
   createGameType,
   getAllGameTypes,
-  // getGameTypeByGameType,
   updateGameType,
   deleteGameType,
   getGameTypeByGameTypeId,
